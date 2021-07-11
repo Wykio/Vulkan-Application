3f67fb39d9b4407b194034d1a105d0d6dcc61416
Antoine <antoine.thenevin@gmail.com>
2021-07-11 12:47:59 +0200

    Validation layers

--- Vulkan-Application/Instance.h
+++ Vulkan-Application/Instance.h
@@ -5,10 +5,24 @@
 #include <vector>
 #include <iostream>
 
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 class Instance
 {
     public:
         static void createInstance(VkInstance* instance) {
+            if (enableValidationLayers && checkValidationLayerSupport()) {
+                throw std::runtime_error("validation layers requested, but not available!");
+            }
+
             // Information about the app
             VkApplicationInfo appInfo{};
             appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -42,6 +56,21 @@ class Instance
 
             glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
+            checkGLFWextensionSupport(glfwExtensions, glfwExtensionCount, extensions);
+
+            createInfo.enabledExtensionCount = glfwExtensionCount;
+            createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+            // For validation layer
+            createInfo.enabledLayerCount = 0;
+
+            // Finally create the instance
+            if (vkCreateInstance(&createInfo, nullptr, instance) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create instance!");
+            }
+        }
+    private:
+        static void checkGLFWextensionSupport(const char** glfwExtensions, uint32_t glfwExtensionCount, std::vector<VkExtensionProperties> extensions) {
             // Check if every GLFW extension is supported
             std::cout << "glfw extensions:\n";
             for (size_t i = 0; i < glfwExtensionCount; i++)
@@ -58,16 +87,31 @@ class Instance
                 if (supported = false)
                     throw std::runtime_error("A GLFW extention is not supported !");
             }
-
-            createInfo.enabledExtensionCount = glfwExtensionCount;
-            createInfo.ppEnabledExtensionNames = glfwExtensions;
-
-            // For validation layer
-            createInfo.enabledLayerCount = 0;
-
-            // Finally create the instance
-            if (vkCreateInstance(&createInfo, nullptr, instance) != VK_SUCCESS) {
-                throw std::runtime_error("failed to create instance!");
-            }
         }
+
+        static bool checkValidationLayerSupport() {
+            uint32_t layerCount;
+            vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+            std::vector<VkLayerProperties> availableLayers(layerCount);
+            vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+            for (const char* layerName : validationLayers) {
+                bool layerFound = false;
+
+                for (const auto& layerProperties : availableLayers) {
+                    if (strcmp(layerName, layerProperties.layerName) == 0) {
+                        layerFound = true;
+                        break;
+                    }
+                }
+
+                if (!layerFound) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
 };
